94652d77c07f09b7728dfc29ea32742907d805da
Your name <username@mailserver.com>
2022-05-06 14:59:37 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -117,12 +117,18 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Print the current node first and then recurse on the children
 	public void preOrder() {
 		preOrderRecurse(root);  
-		System.out.println("PreOrder test commit");
-		
 	}
 	
 	private void preOrderRecurse(BSTNode<T> node) {
-		
+			System.out.print(node + " ");
+			if(node.leftChild != null)
+			{
+				preOrderRecurse(node.leftChild);
+			}
+			if(node.rightChild != null)
+			{
+				preOrderRecurse(node.rightChild);
+			}
 	}
 	
 	//Traverse the tree in an preorder fashion but using a stack
